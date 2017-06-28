08dda966d8b7286455de030cef44a3df66fd7478
Jason Kottler <jason.kottler@skyscanner.net>
2017-06-28 16:56:46 +0100

    Add dynamic templates with handlebars
    End of chapter 43

--- server.js
+++ server.js
@@ -1,19 +1,26 @@
 const express = require('express');
+const hbs = require('hbs');
 
 var app = express();
+
+app.set('view engine', 'hbs');
 
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
   // res.send('<h1>Hello Express!</h1>');
-  res.send({
-    name: 'Jason',
-    likes: ['Games', 'Programming', 'Sleeping'],
+  res.render('index.hbs', {
+    pageTitle: 'Home Page',
+    currentYear: new Date().getFullYear(),
+    welcomeMessage: 'Welcome...to the WORLD OF TOMORROW!',
   });
 });
 
 app.get('/about', (req, res) => {
-  res.send('About Page');
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+    currentYear: new Date().getFullYear(),
+  });
 });
 
 app.get('/bad', (req, res) => {
