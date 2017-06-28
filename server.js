97b8d0ae40f5869442859e21f118fce8311eb881
Jason Kottler <jason.kottler@skyscanner.net>
2017-06-28 16:36:32 +0100

    Create first Express server

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+var app = express();
+
+app.get('/', (req, res) => {
+  // res.send('<h1>Hello Express!</h1>');
+  res.send({
+    name: 'Jason',
+    likes: ['Games', 'Programming', 'Sleeping'],
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.send('About Page');
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Unable to handle request',
+  });
+});
+
+app.listen(3000);
