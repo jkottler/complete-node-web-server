3d7cde4fd9d00d0ee7832a619135ccb0788918c4
Jason Kottler <jason.kottler@skyscanner.net>
2017-06-28 17:10:58 +0100

    Use partials and helpers in handlebars
    Use partials to keep header and footer DRY.
    
    Use helpers to call javascript from templates.
    
    End of chapter 44

--- server.js
+++ server.js
@@ -3,15 +3,22 @@ const hbs = require('hbs');
 
 var app = express();
 
+hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
-
 app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', text => {
+  return text.toUpperCase();
+});
 
 app.get('/', (req, res) => {
   // res.send('<h1>Hello Express!</h1>');
   res.render('index.hbs', {
     pageTitle: 'Home Page',
-    currentYear: new Date().getFullYear(),
     welcomeMessage: 'Welcome...to the WORLD OF TOMORROW!',
   });
 });
@@ -19,7 +26,6 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
     pageTitle: 'About Page',
-    currentYear: new Date().getFullYear(),
   });
 });
 
